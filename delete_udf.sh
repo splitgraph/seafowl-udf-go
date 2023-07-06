daa0bcb53f3b33b663da812a3dba967e81a1ecce
Patrick Skinner <pas8314@gmail.com>
2023-07-05 22:49:57 -0400

    Introduce delete_udf.sh convenience script

---
+++ delete_udf.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+# from: https://dev.to/meleu/how-to-join-array-elements-in-a-bash-script-303a#join-elements-with-a-string
+joinByString() {
+  local separator="$1"
+  shift
+  local first="$1"
+  shift
+  printf "%s" "$first" "${@/#/$separator}"
+}
+
+filename="seafowl-udf-go.wasm"
+function_name="AddInts"
+wasm_export="AddInts"
+return_type="BIGINT"
+input_types=("BIGINT" "BIGINT")
+host="localhost:8080"
+
+curl -i -H "Content-Type: application/json" $host/q -d@- <<EOF
+{"query": "DROP FUNCTION IF EXISTS $function_name(joinByString ' ' ${input_types[@]});"}
+EOF
