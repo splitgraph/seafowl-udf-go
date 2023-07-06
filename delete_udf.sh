e8b9e28fa702c96314d7bdda7ff412ada7bc6a6e
Patrick Skinner <pas8314@gmail.com>
2023-07-05 22:56:16 -0400

    Improve array joiner

--- delete_udf.sh
+++ delete_udf.sh
@@ -1,13 +1,3 @@
-#! /usr/bin/env bash
-# from: https://dev.to/meleu/how-to-join-array-elements-in-a-bash-script-303a#join-elements-with-a-string
-joinByString() {
-  local separator="$1"
-  shift
-  local first="$1"
-  shift
-  printf "%s" "$first" "${@/#/$separator}"
-}
-
 filename="seafowl-udf-go.wasm"
 function_name="AddInts"
 wasm_export="AddInts"
@@ -15,6 +5,9 @@ return_type="BIGINT"
 input_types=("BIGINT" "BIGINT")
 host="localhost:8080"
 
+IFS=", "
+joined_input_types="${input_types[*]}"
+
 curl -i -H "Content-Type: application/json" $host/q -d@- <<EOF
-{"query": "DROP FUNCTION IF EXISTS $function_name(joinByString ' ' ${input_types[@]});"}
+{"query": "DROP FUNCTION IF EXISTS $function_name($joined_input_types);"}
 EOF
