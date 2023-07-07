df0e7b52bdece5e228ad52894249e21519fcba46
Patrick Skinner <pas8314@gmail.com>
2023-07-06 22:24:07 -0400

    Adapt query_udf.sh to conform
    - Now we use downcased function name, update this script
    - Also parse + pass input args properly

--- query_udf.sh
+++ query_udf.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
 
 # Parameters to set
-function_name="AddInts";
+function_name="addints";
 function_arguments="1, 2";
 host="localhost:8080";
+
+echo "SELECT $function_name($function_arguments) AS RESULT;"
 
 curl -i -H "Content-Type: application/json" $host/q -d@- <<EndOfMessage
 {"query": "SELECT $function_name($function_arguments) AS RESULT;"}
