1b579bdf237d6cc20db4afd377e482bbff5eb23b
Patrick Skinner <pas8314@gmail.com>
2023-07-12 15:21:07 -0400

    log out msgpack stuff

--- main.go
+++ main.go
@@ -12,19 +12,13 @@ func doAdd(left, right int64) int64 {
 //export addi64
 func addi64(inputPtr unsafe.Pointer) unsafe.Pointer {
 	wrappedUdf := func(args []interface{}) (interface{}, error) {
-
 		// Decode the arguments
-		left, err := DecodeI64(&args[0])
-		if err != nil {
-			return nil, fmt.Errorf("expected first argument to be int64, got %T", args[0])
-		}
-		right, err := DecodeI64(&args[1])
-		if err != nil {
-			return nil, fmt.Errorf("expected second argument to be int64, got %T", args[1])
-		}
+		left := args[0].(int64)
+		right := args[1].(int64)
 
 		// Call the function
 		result := doAdd(left, right)
+		fmt.Println("result", result)
 
 		// Encode the result
 		return result, nil
